ead16440d247530620966520fae707d29e6fde11
LINESKL <lineskl.aliar@gmail.com>
2026-04-18 11:24:50 +0500

    feat: add Contacts and Registrations screens with mock data and types
    - Created package.json for project dependencies and scripts.
    - Implemented ContactsScreen to display a list of contacts with refresh functionality.
    - Developed RegistrationsScreen for user registration with email validation.
    - Added Contact type definition for better type safety.
    - Introduced mock data generation for contacts.
    - Configured TypeScript settings in tsconfig.json.

---
+++ Lab_14/task_2/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+// Раскомментируйте нужный экран для проверки
+// import ContactsScreen from './src/screens/ContactsScreen';
+import RegistrationScreen from './src/screens/RegistrationsScreen';
+
+export default function App() {
+  // return <ContactsScreen />;
+  return <RegistrationScreen />;
+}
